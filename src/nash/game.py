3e6def88cc60e2e0e8daae16f66d0067b5247556
Vince Knight <vincent.knight@gmail.com>
2016-11-01 14:50:20 +0000

    Implement basic class.

--- src/nash/game.py
+++ src/nash/game.py
@@ -1,1 +1,24 @@
 """A class for a normal form game"""
+import numpy as np
+
+class Game:
+    """
+    A class for a normal form game.
+
+    Parameters
+    ----------
+
+        - A, B: 2 dimensional numpy array representing the payoff matrices for
+          non zero sum games.
+        - A: 2 dimensional numpy array representing the payoff matrix for a
+          zero sum game.
+    """
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.payoff_matrices = args
+        if len(args) == 1:
+            self.payoff_matrices = args[0], -args[0]
+        self.zero_sum = np.array_equal(self.payoff_matrices[0],
+                                       -self.payoff_matrices[1])
+
+
--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -4,13 +4,32 @@ Tests for the game class
 
 import unittest
 import nash
+import numpy as np
 
 class TestGame(unittest.TestCase):
     """
     Tests for the game class
     """
-    def test_init(self):
-        A = [[1, 2], [2, 1]]
-        B = [[2, 1], [1, 2]]
+    def test_bi_matrix_init(self):
+        """Test that can create a bi matrix game"""
+        A = np.array([[1, 2], [2, 1]])
+        B = np.array([[2, 1], [1, 2]])
         g = nash.Game(A, B)
-        self.assertEqual(g.payoffs, (A, B))
+        self.assertEqual(g.payoff_matrices, (A, B))
+        self.assertFalse(g.zero_sum)
+
+    def test_zero_sum_game_init(self):
+        """Test that can create a zero sum game"""
+        A = np.array([[1, 2], [2, 1]])
+        g = nash.Game(A)
+        self.assertTrue(np.array_equal(g.payoff_matrices[0], A))
+        self.assertTrue(np.array_equal(g.payoff_matrices[0],
+                                       -g.payoff_matrices[1]))
+        self.assertTrue(g.zero_sum)
+
+    def test_zero_sum_property_from_bi_matrix(self):
+        """Test that can create a zero sum game"""
+        A = np.array([[1, 2], [2, 1]])
+        B = -A
+        g = nash.Game(A, B)
+        self.assertTrue(g.zero_sum)

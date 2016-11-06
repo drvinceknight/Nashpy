d99a3bba20afc11dad7c60fbeab3cd894313183b
Vince Knight <vincent.knight@gmail.com>
2016-11-06 13:17:24 +0000

    Remove requirements.

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ from setuptools import setup, find_packages
 import unittest
 import doctest
 
-# Read in the requirements.txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 # Read in the version number
 exec(open('src/nash/version.py', 'r').read())
 
@@ -23,7 +19,7 @@ def test_suite():
 setup(
     name='nashpy',
     version=__version__,
-    install_requires=requirements,
+    install_requires=['numpy==1.11.2'],
     author='Vince Knight, James Campbell',
     author_email=('knightva@cardiff.ac.uk'),
     packages=find_packages('src'),

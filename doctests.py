a4d4e638615466e86864243c6205e8c410deee92
Vince Knight <vincent.knight@gmail.com>
2019-11-20 11:10:41 +0000

    Ignore venv if in root strr.

--- doctests.py
+++ doctests.py
@@ -7,13 +7,15 @@ import warnings
 # Note loader and ignore are required arguments for unittest even if unused.
 def load_tests(loader, tests, ignore):
     for root, dirs, files in os.walk("."):
-        for f in files:
-            if f.endswith(".rst") or f.endswith(".md") and ("venv" not in dirs):
-                tests.addTests(
-                    doctest.DocFileSuite(
-                        os.path.join(root, f), optionflags=doctest.ELLIPSIS
+        if "venv" not in root:
+            for f in files:
+                if f.endswith(".rst") or f.endswith(".md"):
+                    print(dirs)
+                    tests.addTests(
+                        doctest.DocFileSuite(
+                            os.path.join(root, f), optionflags=doctest.ELLIPSIS
+                        )
                     )
-                )
 
     return tests
 

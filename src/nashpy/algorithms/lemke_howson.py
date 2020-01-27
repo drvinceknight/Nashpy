d2fd26faeca6a1f86bc06101bc37db4178a935aa
newaij <poontzeyang@gmail.com>
2020-01-28 01:51:04 +0800

    lexicographical lemke howson (rough)
    To do:
    - separate file for lemke howson lex
    - streamline errors
    - general streamlining of code

--- src/nashpy/algorithms/lemke_howson.py
+++ src/nashpy/algorithms/lemke_howson.py
@@ -5,129 +5,254 @@ from itertools import cycle
 import numpy as np
 
 from nashpy.integer_pivoting import (
-    make_tableau,
-    non_basic_variables,
-    pivot_tableau,
+	make_tableau,
+	non_basic_variables,
+	zero_basic_variables,
+	pivot_tableau,
+
+	pivot_tableau_lex
+
 )
 
 
 def shift_tableau(tableau, shape):
-    """
-    Shift a tableau to ensure labels of pairs of tableaux coincide
+	"""
+	Shift a tableau to ensure labels of pairs of tableaux coincide
 
-    Parameters
-    ----------
+	Parameters
+	----------
 
-        tableau: a numpy array
-        shape: a tuple
+		tableau: a numpy array
+		shape: a tuple
 
-    Returns
-    -------
+	Returns
+	-------
 
-        tableau: a numpy array
-    """
-    return np.append(
-        np.roll(tableau[:, :-1], shape[0], axis=1),
-        np.ones((shape[0], 1)),
-        axis=1,
-    )
+		tableau: a numpy array
+	"""
+	return np.append(
+		np.roll(tableau[:, :-1], shape[0], axis=1),
+		np.ones((shape[0], 1)),
+		axis=1,
+	)
 
 
 def tableau_to_strategy(tableau, basic_labels, strategy_labels):
-    """
-    Return a strategy vector from a tableau
+	"""
+	Return a strategy vector from a tableau
 
-    Parameters
-    ----------
+	Parameters
+	----------
 
-        tableau: a numpy array
-        basic_labels: a set
-        strategy_labels: a set
+		tableau: a numpy array
+		basic_labels: a set
+		strategy_labels: a set
 
-    Returns
-    -------
+	Returns
+	-------
 
-        strategy: a numpy array
-    """
-    vertex = []
-    for column in strategy_labels:
-        if column in basic_labels:
-            for i, row in enumerate(tableau[:, column]):
-                if row != 0:
-                    vertex.append(tableau[i, -1] / row)
-        else:
-            vertex.append(0)
-    strategy = np.array(vertex)
-    return strategy / sum(strategy)
+		strategy: a numpy array
+	"""
+	vertex = []
+	for column in strategy_labels:
+		if column in basic_labels:
+			for i, row in enumerate(tableau[:, column]):
+				if row != 0:
+					vertex.append(tableau[i, -1] / row)
+		else:
+			vertex.append(0)
+	strategy = np.array(vertex)
+	return strategy / sum(strategy)
+
+def tableau_to_strategy_lex(tableau, basic_labels, strategy_labels):
+	"""
+	Return a strategy vector from a tableau
+
+	Parameters
+	----------
+
+		tableau: a numpy array
+		basic_labels: a set
+		strategy_labels: a set
+
+	Returns
+	-------
+
+		strategy: a numpy array
+	"""
+	vertex = []
+	for column in strategy_labels:
+		if column in basic_labels:
+			for i, row in enumerate(tableau[:, column]):
+				if row != 0:
+					vertex.append(tableau[i, -1] / row)
+		else:
+			vertex.append(0)
+	strategy = np.array(vertex)
+	return np.divide(strategy, sum(strategy))
 
 
 def lemke_howson(A, B, initial_dropped_label=0):
-    """
-    Obtain the Nash equilibria using the Lemke Howson algorithm implemented
-    using integer pivoting.
+	"""
+	Obtain the Nash equilibria using the Lemke Howson algorithm implemented
+	using integer pivoting.
 
-    Algorithm implemented here is Algorithm 3.6 of [Nisan2007]_.
+	Algorithm implemented here is Algorithm 3.6 of [Nisan2007]_.
 
-    1. Start at the artificial equilibrium (which is fully labeled)
-    2. Choose an initial label to drop and move in the polytope for which
-       the vertex has that label to the edge
-       that does not share that label. (This is implemented using integer
-       pivoting)
-    3. A label will now be duplicated in the other polytope, drop it in a
-       similar way.
-    4. Repeat steps 2 and 3 until have Nash Equilibrium.
+	1. Start at the artificial equilibrium (which is fully labeled)
+	2. Choose an initial label to drop and move in the polytope for which
+	   the vertex has that label to the edge
+	   that does not share that label. (This is implemented using integer
+	   pivoting)
+	3. A label will now be duplicated in the other polytope, drop it in a
+	   similar way.
+	4. Repeat steps 2 and 3 until have Nash Equilibrium.
 
-    Parameters
-    ----------
+	Parameters
+	----------
 
-        initial_dropped_label: int
+		initial_dropped_label: int
 
-    Returns
-    -------
+	Returns
+	-------
 
-        equilibria: A tuple.
-    """
+		equilibria: A tuple.
+	"""
 
-    if np.min(A) <= 0:
-        A = A + abs(np.min(A)) + 1
-    if np.min(B) <= 0:
-        B = B + abs(np.min(B)) + 1
+	if np.min(A) <= 0:
+		A = A + abs(np.min(A)) + 1
+	if np.min(B) <= 0:
+		B = B + abs(np.min(B)) + 1
 
-    # build tableaux
-    col_tableau = make_tableau(A)
-    col_tableau = shift_tableau(col_tableau, A.shape)
-    row_tableau = make_tableau(B.transpose())
-    full_labels = set(range(sum(A.shape)))
+	# build tableaux
+	col_tableau = make_tableau(A)
+	col_tableau = shift_tableau(col_tableau, A.shape)
+	row_tableau = make_tableau(B.transpose())
+	full_labels = set(range(sum(A.shape)))
 
-    if initial_dropped_label in non_basic_variables(row_tableau):
-        tableux = cycle((row_tableau, col_tableau))
-    else:
-        tableux = cycle((col_tableau, row_tableau))
+	if initial_dropped_label in non_basic_variables(row_tableau):
+		tableux = cycle((row_tableau, col_tableau))
+	else:
+		tableux = cycle((col_tableau, row_tableau))
 
-    # First pivot (to drop a label)
-    entering_label = pivot_tableau(next(tableux), initial_dropped_label)
-    while (
-        non_basic_variables(row_tableau).union(non_basic_variables(col_tableau))
-        != full_labels
-    ):
-        entering_label = pivot_tableau(
-            next(tableux), next(iter(entering_label))
-        )
+	# First pivot (to drop a label)
+	entering_label = pivot_tableau(next(tableux), initial_dropped_label)
+	while (
+		non_basic_variables(row_tableau).union(non_basic_variables(col_tableau))
+		!= full_labels
+	):
+		entering_label = pivot_tableau(
+			next(tableux), next(iter(entering_label))
+		)
 
-    row_strategy = tableau_to_strategy(
-        row_tableau, non_basic_variables(col_tableau), range(A.shape[0])
-    )
-    col_strategy = tableau_to_strategy(
-        col_tableau,
-        non_basic_variables(row_tableau),
-        range(A.shape[0], sum(A.shape)),
-    )
+	row_strategy = tableau_to_strategy(
+		row_tableau, non_basic_variables(col_tableau), range(A.shape[0])
+	)
+	col_strategy = tableau_to_strategy(
+		col_tableau,
+		non_basic_variables(row_tableau),
+		range(A.shape[0], sum(A.shape)),
+	)
 
-    if row_strategy.shape != (A.shape[0],) and col_strategy.shape != (
-        A.shape[0],
-    ):
-        msg = """The Lemke Howson algorithm has returned probability vectors of 
+	if row_strategy.shape != (A.shape[0],) and col_strategy.shape != (
+		A.shape[0],
+	):
+		msg = """The Lemke Howson algorithm has returned probability vectors of 
 incorrect shapes. This indicates an error. Your game could be degenerate."""
 
-        warnings.warn(msg, RuntimeWarning)
-    return row_strategy, col_strategy
+		warnings.warn(msg, RuntimeWarning)
+	return row_strategy, col_strategy
+
+def lemke_howson_lex(A, B, initial_dropped_label=0):
+	"""
+	Obtain the Nash equilibria using the Lemke Howson algorithm implemented
+	using integer pivoting.
+
+	Algorithm implemented here is Algorithm 3.6 of [Nisan2007]_.
+
+	1. Start at the artificial equilibrium (which is fully labeled)
+	2. Choose an initial label to drop and move in the polytope for which
+	   the vertex has that label to the edge
+	   that does not share that label. (This is implemented using integer
+	   pivoting)
+	3. A label will now be duplicated in the other polytope, drop it in a
+	   similar way.
+	4. Repeat steps 2 and 3 until have Nash Equilibrium.
+
+	Parameters
+	----------
+
+		initial_dropped_label: int
+
+	Returns
+	-------
+
+		equilibria: A tuple.
+	"""
+
+	if np.min(A) <= 0:
+		A = A + abs(np.min(A)) + 1
+	if np.min(B) <= 0:
+		B = B + abs(np.min(B)) + 1
+
+	# build tableaux
+	col_tableau = make_tableau(A)
+	col_tableau = shift_tableau(col_tableau, A.shape)
+	row_tableau = make_tableau(B.transpose())
+	full_labels = set(range(sum(A.shape)))
+
+#+===========
+	# slack variables
+	row_slack_variables = range(B.shape[0],sum(B.shape))
+	col_slack_variables = range(A.shape[0])
+
+	if initial_dropped_label in non_basic_variables(row_tableau):
+		tableux = cycle((	(row_tableau,row_slack_variables),
+							(col_tableau,col_slack_variables)))
+	else:
+		tableux = cycle((	(col_tableau,col_slack_variables),
+							(row_tableau,row_slack_variables)))
+
+	# First pivot (to drop a label)
+	next_tableux = next(tableux)
+	entering_label = pivot_tableau_lex(next_tableux[0], initial_dropped_label, next_tableux[1])
+	while (
+		non_basic_variables(row_tableau).union(
+				non_basic_variables(col_tableau),
+				zero_basic_variables(row_tableau),
+				zero_basic_variables(col_tableau))
+		!= full_labels
+	):
+		next_tableux = next(tableux)
+		try:
+			entering_label = pivot_tableau_lex(
+				next_tableux[0], next(iter(entering_label)), next_tableux[1]
+			)
+		except StopIteration:
+			msg = """The algorithm has not found a new label after pivoting. 
+			This indicates an error. Your game could be degenerate."""
+			warnings.warn(msg, RuntimeWarning)
+			raise Exception('No new label found. Terminating algorithm.')
+
+	row_strategy = tableau_to_strategy_lex(
+		row_tableau, 
+		full_labels - non_basic_variables(row_tableau) - zero_basic_variables(row_tableau), 
+		range(A.shape[0])
+	)
+	col_strategy = tableau_to_strategy_lex(
+		col_tableau,
+		full_labels - non_basic_variables(col_tableau) - zero_basic_variables(col_tableau),
+		range(A.shape[0], sum(A.shape))
+	)
+	print('col_tableau: \n', col_tableau)
+	print('non_basic_variables(row_tableau) - zero_basic_variables(row_tableau):', non_basic_variables(row_tableau) - zero_basic_variables(row_tableau))
+	print('range(A.shape[0], sum(A.shape)): ', range(A.shape[0], sum(A.shape)))
+
+	if row_strategy.shape != (A.shape[0],) and col_strategy.shape != (
+		A.shape[0],
+	):
+		msg = """The Lemke Howson algorithm has returned probability vectors of 
+incorrect shapes. This indicates an error. Your game could be degenerate."""
+
+		warnings.warn(msg, RuntimeWarning)
+	return row_strategy, col_strategy

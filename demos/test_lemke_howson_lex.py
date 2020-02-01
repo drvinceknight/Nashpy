692bc2a53f67a41a0de3d128f4f4037669bb3c78
newaij <poontzeyang@gmail.com>
2020-02-01 10:21:30 +0800

    moved test_lemke_howson_lex

--- demos/test_lemke_howson_lex.py
+++ demos/test_lemke_howson_lex.py
@@ -1,6 +1,6 @@
 import sys, os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 from nashpy import *
 
 import numpy as np

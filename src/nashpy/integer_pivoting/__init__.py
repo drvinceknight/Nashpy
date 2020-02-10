f6d3d35c8f11a2858b8bfd4a093fc1f1e2d38537
Michalis Panayides <michalis.pan1@hotmail.com>
2020-02-10 13:30:35 +0000

    Manually import instead of import * (#73)
    * Manually import instead of import *
    
    Edit occured in __init__.py files of integer_pivoting, polytope and
    src directories.
    
    * Ensure CI runs on pull request
    
    * Run black

--- src/nashpy/integer_pivoting/__init__.py
+++ src/nashpy/integer_pivoting/__init__.py
@@ -1,1 +1,6 @@
-from .integer_pivoting import *
+from .integer_pivoting import (
+    make_tableau,
+    find_pivot_row,
+    non_basic_variables,
+    pivot_tableau,
+)
--- src/nashpy/polytope/__init__.py
+++ src/nashpy/polytope/__init__.py
@@ -1,1 +1,6 @@
-from .polytope import *
+from .polytope import (
+    build_halfspaces,
+    find_feasible_point,
+    labels,
+    non_trivial_vertices,
+)

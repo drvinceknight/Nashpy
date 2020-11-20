9401c44649fb1a5440d61833b671fb4c8f26b07b
katiemcgoldrick <mcgoldrickkf@cardiff.ac.uk>
2020-11-20 11:17:34 +0000

    Adjust expected values
    note that we had originally written scalar values but the expected
    values are multidimensional.

--- tests/unit/test_replicator_dynamics.py
+++ tests/unit/test_replicator_dynamics.py
@@ -1,4 +1,5 @@
 import nashpy as nash
+import numpy as np
 from nashpy.learning.replicator_dynamics import get_derivative_of_fitness
 
 
@@ -9,9 +10,9 @@ def test_get_derivative_of_fitness():
         np.array([1/2, 1/2, 0]),
         np.array([0, 1/4, 3/4]),
     )
-    derivative_values = (1, 1, 0)
+    derivative_values = (np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([ 0., -0.09375,  0.09375]))
     for x_value, expected_derivative in zip(x_values, derivative_values):
         derivative = get_derivative_of_fitness(x=x_value, t=0, A=M)
-        assert derivative == expected_derivative, x_value
+        assert np.array_equal(derivative, expected_derivative), x_value
 
 

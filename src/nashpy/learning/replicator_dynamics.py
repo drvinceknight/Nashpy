cd258015d8a8045a47f8961befdd4cc95f210e92
katiemcgoldrick <mcgoldrickkf@cardiff.ac.uk>
2020-12-03 16:19:44 +0000

    Updated tests and def replicator_dynamics
    Definition updated for if no y0 given by user.
    Written test for the 2x2, 3x3, and 4x4 cases and tested in case where y0
     given and not given.

--- src/nashpy/learning/replicator_dynamics.py
+++ src/nashpy/learning/replicator_dynamics.py
@@ -11,9 +11,17 @@ def get_derivative_of_fitness(x, t, A):
     return x * (f - phi)
 
 
-def replicator_dynamics(A, y0, timepoints):
+def replicator_dynamics(A, y0, timepoints=None):
     """
     Implement replicator dynamics
     """
+    if timepoints is None:
+        timepoints = np.linspace(0, 10, 1000)
+
+    if y0 is None:
+        y0 = np.zeros((len(A),))
+        for i in range(len(A)):
+            y0[i]= 1/len(A)
+            
     xs = odeint(func=get_derivative_of_fitness, y0=y0, t=timepoints, args=(A,))
     return xs 

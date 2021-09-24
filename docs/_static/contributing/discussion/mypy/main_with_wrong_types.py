a7077bcd2115edc1430e3f718d6f99323c989d8a
Vince Knight <vincent.knight@gmail.com>
2021-09-24 16:12:38 +0100

    Committed documentation

---
+++ docs/_static/contributing/discussion/mypy/main_with_wrong_types.py
@@ -0,0 +1,17 @@
+from typing import Iterable
+
+def get_mean(collection: Iterable) -> float:
+    """
+    Obtain the average of a collection of objects.
+
+    Parameters
+    ----------
+    collection : list
+        A list of numbers
+
+    Returns
+    -------
+    float
+        The mean of the numbers.
+    """
+    return sum(collection) / len(collection)

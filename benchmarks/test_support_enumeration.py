76ee4bb706b82203499677f17acf47c09800ca20
Sankalp Thakur <sankalphimself@gmail.com>
2026-08-20 15:39:29 +0530

    Add larger support enumeration benchmark cases
    Recreate the generator each call and include 5x5, 6x6, and 7x7
    RPS-plus-fill games so the slowdown can be compared at scale.
    
    Signed-off-by: Sankalp Thakur <sankalphimself@gmail.com>

--- benchmarks/test_support_enumeration.py
+++ benchmarks/test_support_enumeration.py
@@ -11,14 +11,12 @@ from nashpy.algorithms.support_enumeration import support_enumeration
 
 def test_support_enumeration_on_two_by_two_game(benchmark):
     A = np.array(((1, -1), (-1, 1)))
-    eqs = support_enumeration(A, -A)
-    benchmark(tuple, eqs)
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
 
 
 def test_support_enumeration_on_three_by_three_game(benchmark):
     A = np.array(((0, 1, -1), (-1, 0, 1), (1, -1, 0)))
-    eqs = support_enumeration(A, -A)
-    benchmark(tuple, eqs)
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
 
 
 def test_support_enumeration_on_four_by_four_game(benchmark):
@@ -30,5 +28,46 @@ def test_support_enumeration_on_four_by_four_game(benchmark):
             (1 / 4, 1 / 4, 1 / 4, 1 / 4),
         )
     )
-    eqs = support_enumeration(A, -A)
-    benchmark(tuple, eqs)
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
+
+
+def test_support_enumeration_on_five_by_five_game(benchmark):
+    A = np.array(
+        (
+            (0, 1, -1, 1 / 4, 1 / 4),
+            (-1, 0, 1, 1 / 4, 1 / 4),
+            (1, -1, 0, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+        )
+    )
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
+
+
+def test_support_enumeration_on_six_by_six_game(benchmark):
+    A = np.array(
+        (
+            (0, 1, -1, 1 / 4, 1 / 4, 1 / 4),
+            (-1, 0, 1, 1 / 4, 1 / 4, 1 / 4),
+            (1, -1, 0, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+        )
+    )
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
+
+
+def test_support_enumeration_on_seven_by_seven_game(benchmark):
+    A = np.array(
+        (
+            (0, 1, -1, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (-1, 0, 1, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1, -1, 0, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+            (1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4),
+        )
+    )
+    benchmark(lambda: tuple(support_enumeration(A, -A)))
